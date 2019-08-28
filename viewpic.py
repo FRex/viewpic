31efb0baa45bf9d2f5d836f08666f08dac6c198a
FRex <rex2k8@gmail.com>
2019-08-28 18:13:04 +0200

    added drag and drop

--- viewpic.py
+++ viewpic.py
@@ -1,41 +1,61 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QFrame
 from PySide2.QtGui import QPixmap
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QUrl
 
 
 class MyPicLabel(QLabel):
-    def __init__(self, pixmap):
+    def __init__(self):
         super(MyPicLabel, self).__init__()
-        self.pixmap = pixmap
+        self.pixmap = None
         self.setMinimumSize(32, 32)
         self.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-        if self.pixmap:
-            self.setPixmap(self.pixmap)
+
+    def setPixmap(self, pixmap):
+        oldpixmap = self.pixmap
+        self.pixmap = pixmap
+        if oldpixmap is None or self.pixmap.isNull():
+            super(MyPicLabel, self).setPixmap(self.pixmap)
+        elif not self.pixmap.isNull():
+            scaled = self.pixmap.scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio)
+            super(MyPicLabel, self).setPixmap(scaled)
 
     def resizeEvent(self, event):
-        if self.pixmap:
-            self.setPixmap(self.pixmap.scaled(event.size(), Qt.AspectRatioMode.KeepAspectRatio))
+        if self.pixmap and not self.pixmap.isNull():
+            scaled = self.pixmap.scaled(event.size(), Qt.AspectRatioMode.KeepAspectRatio)
+            super(MyPicLabel, self).setPixmap(scaled)
 
 
 class MyMainWindow(QMainWindow):
-    def __init__(self, picpath):
+    def __init__(self):
         super(MyMainWindow, self).__init__()
-        self.setWindowTitle(picpath)
+        self.label = MyPicLabel()
         self.setAcceptDrops(True)
-        p = MyPicLabel(QPixmap(picpath))
-        p.setFrameShape(QFrame.Shape.Box)
-        self.setCentralWidget(p)
+        self.setCentralWidget(self.label)
+
+    def openPicture(self, fname):
+        self.setWindowTitle(fname)
+        p = QPixmap(fname)
+        self.label.setPixmap(p)
+        return not p.isNull()
 
     def dragEnterEvent(self, event):
         event.acceptProposedAction()
 
     def dropEvent(self, event):
-        print(event)
+        md = event.mimeData()
+        if md.hasUrls() and len(md.urls()) >= 1:
+            url = md.urls()[0].url()
+            if url.startswith('file:///'):
+                url = url[len('file:///'):]
+            if self.openPicture(url):
+                event.acceptProposedAction()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = MyMainWindow(sys.argv[1])
+    win = MyMainWindow()
+    if len(sys.argv) > 1:
+        win.openPicture(sys.argv[1])
     win.show()
     sys.exit(app.exec_())
